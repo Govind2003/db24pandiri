ac4f9da30141382dabe01ae2c063803ec448c044
Govind2003 <89623058+Govind2003@users.noreply.github.com>
2021-12-01 15:26:54 -0600

    added login an register files in views

--- routes/mug.js
+++ routes/mug.js
@@ -1,6 +1,15 @@
 var express = require('express'); 
 const mug_controlers= require('../controllers/mug'); 
-var router = express.Router(); 
+var router = express.Router();
+
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
  
 /* GET home page. */
 router.get('/', mug_controlers.mug_view_all_Page);
@@ -10,9 +19,9 @@ router.get('/mug/:id', mug_controlers.mug_detail);
 /* GET detail mug page */
 router.get('/detail', mug_controlers.mug_view_one_Page);
 /* GET create mug page */
-router.get('/create', mug_controlers.mug_create_Page);
+router.get('/create', secured, mug_controlers.mug_create_Page);
 /* GET create update page */
-router.get('/update', mug_controlers.mug_update_Page);
+router.get('/update', secured, mug_controlers.mug_update_Page);
 /* GET create mug page */
-router.get('/delete', mug_controlers.mug_delete_Page);
+router.get('/delete', secured, mug_controlers.mug_delete_Page);
 module.exports = router;
